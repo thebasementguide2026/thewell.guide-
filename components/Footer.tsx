37478b5462cff85effd0ceabfd85864751ed110b
thebasementguide2026 <pnason4@gmail.com>
2026-03-26 00:49:01 +0000

    Add No Water from Well to footer Problems column

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,6 +27,7 @@ const footerSections = [
       { label: 'Well Water Smells Like Sulfur', href: '/problems/well-water-smells-like-sulfur' },
       { label: 'Well Water Pressure Low', href: '/problems/well-water-pressure-low' },
       { label: 'Brown or Rusty Well Water', href: '/problems/brown-rusty-well-water' },
+      { label: 'No Water from Well', href: '/problems/no-water-from-well' },
     ],
   },
 ]
