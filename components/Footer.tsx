f5d291f7f5682158a346faf881a627f4a2dc8a71
thebasementguide2026 <pnason4@gmail.com>
2026-03-27 02:39:11 +0000

    Add buying house with well to footer Guides column

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,6 +12,7 @@ const footerSections = [
       { label: 'Well Water Contaminants', href: '/guides/well-water-contaminants' },
       { label: 'Well Pressure Tank Guide', href: '/guides/well-pressure-tank-guide' },
       { label: 'Well Water Treatment Options', href: '/guides/well-water-treatment-options' },
+      { label: 'Buying a House With a Well', href: '/guides/buying-house-with-well' },
     ],
   },
   {
