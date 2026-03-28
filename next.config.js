7bd335ffb09d1fa1829d64e505acc8b516dc26c3
thebasementguide2026 <pnason4@gmail.com>
2026-03-28 11:21:34 +0000

    add 301 redirects for 3 dead slugs to their live equivalents

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/guides/well-water-testing',
+        destination: '/guides/how-to-test-well-water',
+        permanent: true,
+      },
+      {
+        source: '/problems/well-water-pressure-problems',
+        destination: '/problems/well-water-pressure-low',
+        permanent: true,
+      },
+      {
+        source: '/reviews/best-well-water-filters',
+        destination: '/reviews/best-whole-house-water-filters',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
